0d4885f9a29e78fcb808baad379c41ab06b55914
Carlos Menezes <menezesoliveira40@hotmail.com>
2020-01-13 19:21:24 -0300

    4.2 exercicio - criando o catálogo de filmes e tela de detalhes do filme com sessões para compra

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -25,6 +26,11 @@ public class SessaoDao {
 	public List<Sessao> buscaSessaoDaSala(Sala sala){
 		return manager.createQuery("select s from Sessao s Where s.sala = :sala",
 				Sessao.class).setParameter("sala", sala).getResultList();
+	}
+	
+	public List<Sessao> buscaSessoesDoFilme(Filme filme){
+		return manager.createQuery("select s from Sessao s Where s.filme = :filme",
+				Sessao.class).setParameter("filme", filme).getResultList();
 	}
 	
 }
